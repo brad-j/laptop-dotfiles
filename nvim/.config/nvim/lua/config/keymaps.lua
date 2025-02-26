e5d09ea80fd6149bc38d3e365f36f70db211a174
brad <brad@bradjohnson.io>
2025-02-26 12:40:43 -0500

    Sick nvim stuff

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,8 @@
 local map = vim.keymap.set
 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 -- Map jk to esc
 map('i', 'jk', '<esc>l', defaults)
 
@@ -9,3 +12,19 @@ map('n', '<Leader>w', ':write<CR>')
 map('n', '<Leader>a', ':wqa<CR>')
 map('n', '<Leader>x', ':wq<CR>')
 
+map('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -9,3 +9,26 @@ opt.tabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
 opt.softtabstop = 2
+
+opt.mouse = 'a'
+
+vim.schedule(function()
+  opt.clipboard = 'unnamedplus'
+end)
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.signcolumn = 'yes'
+
+opt.updatetime = 250
+opt.timeoutlen = 300
+
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣'}
+
+opt.inccommand = 'split'
+
+opt.cursorline = true
+
+opt.scrolloff = 10
